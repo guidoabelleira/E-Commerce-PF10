0b153df9a81388d9ad74e0f4e67f385c80f5e585
Facundo Sanchez <sanchezfacundocristian@hotmail.com>
2021-09-28 21:09:35 -0300

    fixed an error

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -7,10 +7,10 @@ export const exampleFunction = example =>{
     }
 }
 
-export function getProductByName(name) {
+export function getProductByName(input) {
     return async function(dispatch) {
         try {
-            let json = await axios.get('' + name)
+            let json = await axios.get('' + input)
             return dispatch ({
                 type: 'GET_RECIPE_BY_NAME',
                 payload: json.data
