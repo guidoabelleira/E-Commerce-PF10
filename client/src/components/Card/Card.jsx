eef22602266200d2aa67e57d2626f9c83d6f3349
Alejandro Motta <alejandromotta808@gmail.com>
2021-09-29 13:35:00 -0500

    Alejandro Motta

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,11 +1,18 @@
-import { Link } from "react-router-dom"
-import React, { useState } from "react";
+import { NavLink as Link } from 'react-router-dom';
+import style from './Card.module.css'
 
-export default function Card ({props}=props) {
-    console.log (props)
-    return (
-        <div>
-            <h1>{props.texto}</h1>
+export default function Card ({image, name, category,id,price, index}){
+    return(
+        <div className={style.container} key={index}>
+            <Link to={`/${id}`}> 
+                <img className={style.img}src={image} alt={name} height="300px" width="350px" />
+            </Link>
+
+            <h4 className={style.h4}>{name}</h4>
+            {category.map((e, i)=> {
+                return <p className={style.p} key={i}>{e.name}</p>
+            })}
+            <p> {price}</p>
         </div>
-    )
+)
 }
--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -1,15 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { store } from '../../redux/store'
-import Card from '../Card/Card'
+import Card from "../Card/Card"
+import style from "./Cards.module.css"
 
-export default function Cards () {
-    const props = store.getState()
-    console.log( props )
-    let texto = props.reducer
-    const id = 1
-    return (
-        <div>
-            <Card key={id} props={texto} />
-        </div>
-    )
+export default function Cards ({state}) {
+    if(state[0] !== {error: "Product not found"} && state[0]){
+       return(<div className={style.container}>
+       <div className={style.parent}>{ state.map((e, i) => {
+           if(e.name){
+            return (<div key={i}><Card 
+                id={e.id}
+                image={e.image}
+                name={e.name}
+                category={e.category}
+                price={e.price}
+                index={i} />
+                </div>
+         )
+           } else{
+               return <div key={i}><p>error</p></div>;
+           }
+       
+    })}</div></div>) 
+    } else {
+        //si se puede en el futuro remplazar por un gif de carga
+        return (
+            <div>
+                <p>No Products Found.</p>
+            </div>
+        )
+    }
 }
