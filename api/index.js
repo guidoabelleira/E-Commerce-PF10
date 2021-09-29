2b6204e62e5015c29bb492c9732a3cba60cc4ca0
Lucas Ruiz <79382758+lucasruiz12@users.noreply.github.com>
2021-09-29 19:37:55 -0300

    loader en index con force true

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const { conn } = require('./src/db.js');
 const { loaderDB } = require('./src/loader/loader')
 
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
     loaderDB();
     console.log('%s listening at 3001');
