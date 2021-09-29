d89d968a3be822c096cd0cce2b1b4f13e3794129
guidoabelleira <guidoabelleira@gmail.com>
2021-09-29 17:09:17 -0300

    Se comienza con popularProducts, discountProducts, lastAdded

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,17 +1,47 @@
-import {useEffect, useState} from 'react';
-import NavBar from '../NavBar';
+import { useEffect, useState } from 'react';
+import NavBar from '../NavBar/NavBar';
+import SearchBar from '../Searchbar';
+import Cards from '../Cards/Cards';
+import { useDispatch, useSelector } from 'react-redux';
 
 import style from './home.module.css';
 
 
-function Home(props) {
+function Home() {
+    const dispatch = useDispatch();
+    const allProducts = useSelector(state => state.product)
+
+    const date //esta constante hace referencia a la fecha actual
+
+    const popularProducts // filtramos productos por los mas vendidos - FALTA hacer relacion de venta en bd para poder filtrar
+    const discountProducts = allProducts.filter(p => p.onSale !== false); // filtramos productos con descuento en base al valor onSale
+    const lastAdded = allProducts.filter(p => p.upCreateDate < date); // filtramos prod por fecha de creacion
+
+    useEffect(() => {
+        async function geters() {
+            await dispatch(getAllProducts());
+        }
+        geters();
+    }, []);
+
 
     return (
         <div className={style.body}>
-            <NavBar />
-            {/* <Search /> */}
-           
+            <div className={style.navegate}>
+                <NavBar />
+                <SearchBar /> 
+            </div>
+            
+            <div className={style.bodyCards}>
+                <Cards popularProducts={popularProducts}/>
+                <Cards discountProducts={discountProducts}/>
+                <Cards lastAdded={lastAdded}/>
+            </div>
+            <div className={style.startedPack}>
+                aca va startedPack
+            </div>
         </div>
+        
     )
 };
 
