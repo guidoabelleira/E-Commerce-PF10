e35971b9a136211f757db4dcbd809d00e8b5d44d
CePersico <cepersico.3@gmail.com>
2021-09-29 15:53:10 -0300

    ruta get

---
+++ api/src/controllers/controllers.js
@@ -0,0 +1,49 @@
+const {Product, Categories, category_product} = require('../db');
+//const axios = require('axios');
+//const data = require('./data.json');
+
+  const getAllInfo = async () => {
+     // return await data
+    return await Product.findAll({      
+     /*  include: {
+        model: Categories, 
+        attributes: ["name"], 
+        through: {
+          attributes: [],
+        },
+      }, */
+    });
+  }; 
+  
+
+  /* const createProduct = async (
+    name,
+  ) => {
+    try {
+      const id = uuidv4();
+      const newProduct = await Product.create({
+        name,
+        image,
+        price,
+        stock,
+        onStock,
+        onSale,
+        description,
+        id: id
+      });
+  
+      const categoriesDb = await Categories.findAll({
+        where: { name: categories },
+      });
+  
+      await newProduct.addType(categoriesDb);
+      return newProduct;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+   */
+  module.exports = {
+	getAllInfo, 
+    //createProduct
+};
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,20 @@
 const { Router } = require('express');
+const axios = require("axios");
+const { Product, Categories, category_product } = require("../db.js");
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+const productsRoute = require('./products');
+//const categoryRoute = require('./category');
 
+router.use('/products', productsRoute);
+//router.use('/category', categoryRoute );
 
 module.exports = router;
+
+
