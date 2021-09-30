a1d2a2d86ddf0ccab11f516ecb1e6d8686a98859
Pía Correa <pia.correac@usach.cl>
2021-09-29 22:20:53 -0300

    Add id, put and delete in productRoutes

--- api/src/routes/products.js
+++ api/src/routes/products.js
@@ -4,10 +4,12 @@ const {Product, Categories} = require('../db');
 const { getAllInfo, /* createProduct */} = require('../controllers/controllers');
 const data = require('../../data.json');
 
+//Realizado por Cecilia
 router.get("/json", async (req, res, next) => {
         res.json(data);
 });
 
+//Realizado por Cecilia
 router.get("/", async (req, res, next) => { 
     const name  = req.query.name; 
     try {
@@ -27,6 +29,48 @@ router.get("/", async (req, res, next) => {
         next(err);
     }
 });
+
+//Realizado por Pía
+router.get('/:id', async (req, res) => {
+    const {id} = req.params;
+   try {
+     const productsTotal = await getAllInfo();
+     if (id){
+       let productId = await productsTotal.filter(el => el.id == id);
+       productId.length ? res.status(200).json( productId) : res.json({data: {error:'No se encontró el producto requerido'}})             
+     }
+   } catch (error) {
+     console.log(err);
+   }
+   })
+
+//Realizado por Pía
+router.delete ('/product', async (req, res) => {    
+ let {name} = req.body;   
+  await Product.destroy({   
+        where: {                                            
+          name
+        }
+   })
+ res.status(200).send('Producto eliminado con éxito')
+}) 
+
+//Realizado por Pía
+router.put('/:id', async (req, res, next) => {
+  let {id} = req.params
+   let product = req.body;
+   try {
+       let productDb = await Product.update(product, {   
+              where: {
+                id: id
+              }
+       })
+        return res.status(200).json({cambiado: true})
+       
+   } catch (err) {
+       next(err);
+   } 
+})
 
 
 module.exports = router;
