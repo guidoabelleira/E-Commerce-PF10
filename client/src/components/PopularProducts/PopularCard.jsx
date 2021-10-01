5dcb2c639a24403ed1eb9bcdf65aa78a3471d91e
Alejandro Motta <alejandromotta808@gmail.com>
2021-10-01 09:56:21 -0500

    Cards Creadas, falta css y hacer el shopcart

--- client/src/components/PopularProducts/PopularCard.jsx
+++ client/src/components/PopularProducts/PopularCard.jsx
@@ -1,13 +1,12 @@
 import { NavLink as Link } from 'react-router-dom';
-import style from './DiscountCard.module.css'
+import style from './PopularCard.module.css'
  
-export default function DiscountCard ({image, name, category,id,index}){
+export default function PopularCard ({image, name, category,id,price, index}){
     return(
         <div className={style.container} key={index}>
-            <p>20% discount</p>
-            <Link to={`/products`}> <button>
-            <p>see all</p>
-            </button>
+            <p>Popular Products</p>
+            <Link to={"/products"}> <button>
+            <p>see all</p></button>
             </Link>
             <Link to={`/${id}`}> <img className={style.img}src={image} alt={name} height="300px" width="350px" />
             </Link>
@@ -16,9 +15,9 @@ export default function DiscountCard ({image, name, category,id,index}){
             {category.map((e, i)=> {
             return <p className={style.p} key={i}>{e.name}</p>
             })}
-            <Link to={`/${id}`}> <button>
-            <p>Shop Now</p></button>
-            </Link>
+            <button>
+                <p>+ ${price}</p>
+            </button>
         </div>
 )
 }
