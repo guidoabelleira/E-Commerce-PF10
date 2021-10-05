793294e19baa81f6ce3f7e5e82c61767e0f3cbcb
Alejandro Motta <alejandromotta808@gmail.com>
2021-10-05 14:04:45 -0500

    css y algunos componentens

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -6,16 +6,28 @@ import { useDispatch, useSelector } from 'react-redux';
 export default function Card ({image, name, categories,id,price, index}){
     const dispatch = useDispatch()
     const product = {image, name, categories, id, price}
-    
+    let duplicate = name.split(" ")
+    console.log(name)
+    name = [];
+     name.push(duplicate[0])
+     name.push(" ")
+     name.push(duplicate[1])
+     name.push(" ")
+     name.push(duplicate[2])
+     console.log(name)
+     name.join("")
     return(
         <div className={style.container} key={index}>
         <Link className={style.bttn}to={`/products/${id}`}>  <img className={style.img}src={image} alt={name} height="300px" width="350px" />
          </Link>
 
          <h4 className={style.h4}>{name}</h4>
+         <div className={style.categories}>
          {categories.map((e, i)=> {
          return <p className={style.p} key={i}>{e.name}</p>
          })}
+         </div>
+         
              <div onClick={e => dispatch(addPCart(product))} className={style.shop}>
                  <div className={style.circle}>
                      <p>+</p>
