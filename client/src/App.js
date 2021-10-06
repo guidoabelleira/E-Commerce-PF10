d11424a98a1b217c149284175c0634dd2322c5f3
guidoabelleira <guidoabelleira@gmail.com>
2021-10-05 22:22:56 -0300

    Creacion deContactenos

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
-
+import {useAuth0} from "@auth0/auth0-react"
 
 import HomePage from './components/HomePage/HomePage';
 import NavBar from './components/NavBar/NavBar';
@@ -9,8 +9,9 @@ import DetailCard from './components/detailCard/detailCard';
 import ShopCart from './components/ShopCart/ShopCart';
 import AddProduct from './components/CreateProductForm/Form';
 import AboutUs from './components/AboutUs/AboutUs';
-import {useAuth0} from "@auth0/auth0-react"
 import Profile from "./components/Profile/Profile"
+import ContactUs from './components/ContactUs/ContactUs';
+
 import './App.css';
 
 function App() {
@@ -37,6 +38,7 @@ function App() {
                  <Route exact path={"/products/:id"} component={DetailCard} />
                  <Route exact path={"/shopcart"} component={ShopCart} />
                  <Route exact path={"/about"} component={AboutUs} />
+                 <Route exact path={"/contact"} component={ContactUs} />
                  </>)}
         </div>
         </React.Fragment>
