aee3c323cd32a4c236cb656a56e9ca0cdf41b065
Lucas Ruiz <79382758+lucasruiz12@users.noreply.github.com>
2021-10-13 16:07:15 -0300

    superadmin and loader

--- api/index.js
+++ api/index.js
@@ -1,12 +1,13 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const { loaderProducts, loaderCategory } = require('./src/loader/loader')
+const { loaderProducts, loaderCategory, loaderAdmin } = require('./src/loader/loader')
 
 
 conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, async () => {
     await loaderCategory();
     await loaderProducts();
+/*     await loaderAdmin(); */
     console.log('%s listening at 3001');
   });
 });
