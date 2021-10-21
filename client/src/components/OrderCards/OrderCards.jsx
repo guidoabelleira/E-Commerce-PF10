eaf83dd180d7f7c8966abd8c667cf0258098342a
Argenthands <alejandro.david.benolol@gmail.com>
2021-10-21 11:06:48 +0300

    conected addReview to OrderCards

--- client/src/components/OrderCards/OrderCards.jsx
+++ client/src/components/OrderCards/OrderCards.jsx
@@ -1,5 +1,5 @@
 import OrderCard from '../OrderCard/OrderCard';
-
+import { Link } from 'react-router-dom';
 
 import style from './orderCards.module.css';
 
@@ -10,26 +10,28 @@ export default function OrderCards ({state}){
 
     return state[0] ? (
         <div className={style.container}>
-            <div className={style.parent}>
 
+            <div className={style.parent}>
                 {state.map((e, i) => {
                     return (
                         <div className={style.cards} key={i}>
                             <OrderCard id={e.id} totalPrice={e.totalPrice} createdAt={e.createdAt} index={i}/>
                         </div>
                     )
-                }
-
-                )}
-
+                })}
             </div>
 
-
-
+            <Link 
+                className={style.bttn}
+                to={'/adminproducts/PreLoadAddReview'}
+            >
+                <b>Hacer una reseña</b>
+            </Link>
         </div>
         
-        
-    ) : (
+    )
+    : 
+    (
         <p>Aun no hay datos</p>
     )
 }
